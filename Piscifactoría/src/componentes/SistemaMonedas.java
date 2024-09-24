b698c8c567c9252a5780ecfb442c2de5c2dc7b26
Lois Domínguez Domínguez <stilton1267@gmail.com>
2024-09-24 16:44:57 +0200

    Componente del sistema de monedas implementado.

---
+++ Piscifactoría/src/componentes/SistemaMonedas.java
@@ -0,0 +1,45 @@
+package componentes;
+
+/**
+ * Clase que representa a un sistema en el se dispone de una cantidad de monedas.
+ */
+public class SistemaMonedas {
+    
+    /**
+     * Monedas de las que se dispone.
+     */
+    private int monedas;
+
+    /**
+     * Constructor que inicia el sistema de monedas con una cantidad nula de monedas.
+     */
+    public SistemaMonedas(){
+        this.monedas = 0;
+    }
+
+    /**
+     * Constructor parametrizado.
+     * @param monedas Cantidad de monedas de las que se dispone inicialmente en el sistema de monedas.
+     */
+    public SistemaMonedas(int monedas){
+        super();
+        this.monedas = monedas;
+    }
+
+    /**
+     * 
+     * @return Monedas de las que se dispone en el sistema de monedas.
+     */
+    public int getMonedas() {
+        return monedas;
+    }
+
+    /**
+     * Establece la cantidad de monedas de las que se dispone en el sistema de monedas.
+     * @param monedas Número de monedas a establecer.
+     */
+    public void setMonedas(int monedas) {
+        this.monedas = monedas;
+    }
+
+}
