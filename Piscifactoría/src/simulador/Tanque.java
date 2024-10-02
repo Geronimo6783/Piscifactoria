0942071a2ec2d06d778a782873467cab51605215
ldominguez <ldominguez@iescotarelo.org>
2024-10-02 11:23:37 +0200

    Inicio del desarrollo de tanque

--- Piscifactoría/src/simulador/Tanque.java
+++ Piscifactoría/src/simulador/Tanque.java
@@ -1,5 +1,13 @@
 package simulador;
 
+import java.util.ArrayList;
+
 public class Tanque {
 
+    /**
+     * Capacidad máxima de peces que puede tener el tanque.
+     */
+    int capacidadMaximaPeces; 
+
+    
 }
