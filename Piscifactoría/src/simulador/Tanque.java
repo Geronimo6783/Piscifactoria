d9aaa1ba7e8a8e7a22f4e9bcd3bc26057d249906
Lois Domínguez Domínguez <stilton1267@gmail.com>
2024-10-14 17:51:16 +0200

    Métodos hayMachoFertil() y hayHembraFertil() añadidos y método alimentarAleatorio() finalizado en la clase tanque

--- Piscifactoría/src/simulador/Tanque.java
+++ Piscifactoría/src/simulador/Tanque.java
@@ -20,7 +20,7 @@ public class Tanque {
     /**
      * Capacidad máxima de peces que puede tener el tanque.
      */
-    private int capacidadMaximaPeces; 
+    private int capacidadMaximaPeces;
 
     /**
      * Peces del tanque.
@@ -52,7 +52,9 @@ public class Tanque {
 
     /**
      * Permite establecer la capacidad máxima de peces que puede tener el tanque.
-     * @param capacidadMaximaPeces Capacidad máxima de peces que puede tener el tanque a establecer.
+     * 
+     * @param capacidadMaximaPeces Capacidad máxima de peces que puede tener el
+     *                             tanque a establecer.
      */
     public void setCapacidadMaximaPeces(int capacidadMaximaPeces) {
         this.capacidadMaximaPeces = capacidadMaximaPeces;
@@ -68,6 +70,7 @@ public class Tanque {
 
     /**
      * Permite establecer los peces del tanque.
+     * 
      * @param peces Peces del tanque a establecer.
      */
     public void setPeces(ArrayList<Pez> peces) {
@@ -84,7 +87,9 @@ public class Tanque {
 
     /**
      * Permite establecer la capacidad máxima de comida por tipo.
-     * @param capacidadMaximaComida Capacidad máxima de comida por tipo a establecer.
+     * 
+     * @param capacidadMaximaComida Capacidad máxima de comida por tipo a
+     *                              establecer.
      */
     public void setCapacidadMaximaComida(int capacidadMaximaComida) {
         this.capacidadMaximaComida = capacidadMaximaComida;
@@ -100,6 +105,7 @@ public class Tanque {
 
     /**
      * Permite establecer la cantidad de comida animal disponible.
+     * 
      * @param comidaAnimal Cantidad de comida animal a establecer.
      */
     public void setComidaAnimal(int comidaAnimal) {
@@ -108,7 +114,7 @@ public class Tanque {
 
     /**
      * 
-     * @return Cantidad de comidad vegatal disponible 
+     * @return Cantidad de comidad vegatal disponible
      */
     public int getComidaVegetal() {
         return comidaVegetal;
@@ -116,6 +122,7 @@ public class Tanque {
 
     /**
      * Permite establecer la cantidad de comida vegetal disponible.
+     * 
      * @param comidaVegetal Cantidad de comida vegetal disponible a establecer.
      */
     public void setComidaVegetal(int comidaVegetal) {
@@ -132,9 +139,10 @@ public class Tanque {
 
     /**
      * Constructor de tanques.
+     * 
      * @param numeroTanque Número del tanque.
      */
-    public Tanque(int numeroTanque){
+    public Tanque(int numeroTanque) {
         capacidadMaximaComida = 200;
         comidaAnimal = 200;
         comidaVegetal = 200;
@@ -144,26 +152,30 @@ public class Tanque {
     /**
      * Imprime el estado del tanque por pantalla.
      */
-    public void showStatus(){
-        System.out.println("=============== Tanque ===============\nOcupación: " + peces.size() + " / " + capacidadMaximaPeces + "(" +String.format("%.2f", ((float)peces.size())/(float)capacidadMaximaPeces)
-                            +")\nPeces vivos: " + pecesVivos() + " / " + peces.size() + "(" +String.format("%.2f", ((float)pecesVivos()/(float)peces.size())) 
-                            + ")\nPeces alimentados: " + pecesAlimentados() + " / " + peces.size() + "(" + String.format("%.2f", ((float)pecesAlimentados()/(float)peces.size())) 
-                            + ")\nPeces adultos: " + pecesAdultos() + " / " + peces.size() + "(" + String.format("%.2f", ((float)pecesAdultos()/(float)peces.size())) 
-                            + ")\nHembras / Machos: " + pecesHembra() + " / " + pecesMacho() 
-                            + "\nFértiles: " + pecesFertiles() + " / " + pecesVivos()
-                            );
-        
+    public void showStatus() {
+        System.out.println(
+                "=============== Tanque ===============\nOcupación: " + peces.size() + " / " + capacidadMaximaPeces
+                        + "(" + String.format("%.2f", ((float) peces.size()) / (float) capacidadMaximaPeces)
+                        + ")\nPeces vivos: " + pecesVivos() + " / " + peces.size() + "("
+                        + String.format("%.2f", ((float) pecesVivos() / (float) peces.size()))
+                        + ")\nPeces alimentados: " + pecesAlimentados() + " / " + peces.size() + "("
+                        + String.format("%.2f", ((float) pecesAlimentados() / (float) peces.size()))
+                        + ")\nPeces adultos: " + pecesAdultos() + " / " + peces.size() + "("
+                        + String.format("%.2f", ((float) pecesAdultos() / (float) peces.size()))
+                        + ")\nHembras / Machos: " + pecesHembra() + " / " + pecesMacho()
+                        + "\nFértiles: " + pecesFertiles() + " / " + pecesVivos());
+
     }
 
     /**
      * 
      * @return Número de peces vivos en el tanque.
      */
-    private int pecesVivos(){
+    private int pecesVivos() {
         int pecesVivos = 0;
 
-        for(Pez pez :  peces){
-            if(pez.isVivo()){
+        for (Pez pez : peces) {
+            if (pez.isVivo()) {
                 pecesVivos++;
             }
         }
@@ -175,11 +187,11 @@ public class Tanque {
      * 
      * @return Número de peces alimentados en el tanque.
      */
-    private int pecesAlimentados(){
+    private int pecesAlimentados() {
         int pecesAlimentados = 0;
 
-        for(Pez pez :  peces){
-            if(pez.isAlimentado()){
+        for (Pez pez : peces) {
+            if (pez.isAlimentado()) {
                 pecesAlimentados++;
             }
         }
@@ -191,11 +203,11 @@ public class Tanque {
      * 
      * @return Número de peces vivos en el tanque.
      */
-    private int pecesAdultos(){
+    private int pecesAdultos() {
         int pecesAdultos = 0;
 
-        for(Pez pez :  peces){
-            if(pez.isMaduro()){
+        for (Pez pez : peces) {
+            if (pez.isMaduro()) {
                 pecesAdultos += 1;
             }
         }
@@ -207,11 +219,11 @@ public class Tanque {
      * 
      * @return Número de peces macho en el tanque.
      */
-    private int pecesMacho(){
+    private int pecesMacho() {
         int pecesMacho = 0;
 
-        for(Pez pez : peces){
-            if(!pez.isSexo()){
+        for (Pez pez : peces) {
+            if (!pez.isSexo()) {
                 pecesMacho++;
             }
         }
@@ -223,11 +235,11 @@ public class Tanque {
      * 
      * @return Número de peces hembra en el tanque.
      */
-    private int pecesHembra(){
+    private int pecesHembra() {
         int pecesHembra = 0;
 
-        for(Pez pez : peces){
-            if(pez.isSexo()){
+        for (Pez pez : peces) {
+            if (pez.isSexo()) {
                 pecesHembra++;
             }
         }
@@ -239,11 +251,11 @@ public class Tanque {
      * 
      * @return Número de peces fértiles en el tanque.
      */
-    private int pecesFertiles(){    
+    private int pecesFertiles() {
         int pecesFertiles = 0;
 
-        for(Pez pez : peces){
-            if(pez.isFertil()){
+        for (Pez pez : peces) {
+            if (pez.isFertil()) {
                 pecesFertiles++;
             }
         }
@@ -254,102 +266,183 @@ public class Tanque {
     /**
      * Muestra el estado de cada pez del tanque por pantalla.
      */
-    public void showFishStatus(){
-        for(Pez pez : peces){
+    public void showFishStatus() {
+        for (Pez pez : peces) {
             pez.showStatus();
         }
     }
 
     /**
      * Imprime el porcentaje de capacidad del tanque.
+     * 
      * @param piscifactoria Nombre de la piscifactoría en la que se situa el tanque.
      */
-    public void showCapacity(String piscifactoria){
-        System.out.println("Tanque " + numeroTanque + " de la piscifactoría " + piscifactoria + " al " + String.format("%.2f", ((float)peces.size()/(float)capacidadMaximaPeces)) + "% de capacidad.");
+    public void showCapacity(String piscifactoria) {
+        System.out.println("Tanque " + numeroTanque + " de la piscifactoría " + piscifactoria + " al "
+                + String.format("%.2f", ((float) peces.size() / (float) capacidadMaximaPeces)) + "% de capacidad.");
     }
 
-    public void alimentar(){
-        int comidadNecesaria = 0;
-        ArrayList<Integer> cantidadDeComidadNecesariaPorPez = new ArrayList<>();
+    /**
+     *  Gestiona la lógica para alimentar a los peces.
+     */
+    public void alimentar() {
+        int comidaNecesaria = 0;
+        ArrayList<Integer> cantidadDeComidaNecesariaPorPez = new ArrayList<>();
 
-        for(Pez pez : peces){
-            if(pez.isVivo() && !pez.isAlimentado()){
-                cantidadDeComidadNecesariaPorPez.add(pez.comer());
+        for (Pez pez : peces) {
+            if (pez.isVivo() && !pez.isAlimentado()) {
+                cantidadDeComidaNecesariaPorPez.add(pez.comer());
+            } else {
+                cantidadDeComidaNecesariaPorPez.add(0);
             }
         }
 
-        for(Integer cantidadComida : cantidadDeComidadNecesariaPorPez){
-            comidadNecesaria += cantidadComida;
+        for (Integer cantidadComida : cantidadDeComidaNecesariaPorPez) {
+            comidaNecesaria += cantidadComida;
         }
 
-        if(peces.get(0) instanceof Carnivoro){
-            if(comidaAnimal >= comidadNecesaria){
-                comidaAnimal -= comidadNecesaria;
-                for(Pez pez : peces){
-                    if(pez.isVivo() && !pez.isAlimentado()){
+        if (peces.get(0) instanceof Carnivoro) {
+            if (comidaAnimal >= comidaNecesaria) {
+                comidaAnimal -= comidaNecesaria;
+                for (Pez pez : peces) {
+                    if (pez.isVivo() && !pez.isAlimentado()) {
                         pez.setAlimentado(true);
                     }
                 }
+            } else{
+                alimentarAleatorio(cantidadDeComidaNecesariaPorPez, comidaAnimal);
             }
-        }
-        else{
-            if(peces.get(0) instanceof Filtrador){
-                if(comidaVegetal >= comidadNecesaria){
-                    comidaVegetal -= comidadNecesaria;
-                    for(Pez pez : peces){
-                        if(pez.isVivo() && !pez.isAlimentado()){
+        } else {
+            if (peces.get(0) instanceof Filtrador) {
+                if (comidaVegetal >= comidaNecesaria) {
+                    comidaVegetal -= comidaNecesaria;
+                    for (Pez pez : peces) {
+                        if (pez.isVivo() && !pez.isAlimentado()) {
                             pez.setAlimentado(true);
                         }
                     }
-                } 
-            }
-            else{
-                if((comidaVegetal + comidaAnimal) >= comidadNecesaria){
-                    if(comidaVegetal > comidaAnimal){
-                        if(comidaVegetal >= comidadNecesaria){
-                            comidaVegetal -= comidadNecesaria;
-                        }
-                        else{
-                            comidaVegetal -= comidadNecesaria;
+                } else{
+                    alimentarAleatorio(cantidadDeComidaNecesariaPorPez, comidaVegetal);
+                }
+            } else {
+                if ((comidaVegetal + comidaAnimal) >= comidaNecesaria) {
+                    if (comidaVegetal > comidaAnimal) {
+                        if (comidaVegetal >= comidaNecesaria) {
+                            comidaVegetal -= comidaNecesaria;
+                        } else {
+                            comidaVegetal -= comidaNecesaria;
                             comidaAnimal += comidaVegetal;
                             comidaVegetal = 0;
                         }
-                    }
-                    else{
-                        if(comidaAnimal >= comidadNecesaria){
-                            comidaAnimal -= comidadNecesaria;
-                        }
-                        else{
-                            comidaAnimal -= comidadNecesaria;
+                    } else {
+                        if (comidaAnimal >= comidaNecesaria) {
+                            comidaAnimal -= comidaNecesaria;
+                        } else {
+                            comidaAnimal -= comidaNecesaria;
                             comidaVegetal += comidaAnimal;
                             comidaAnimal = 0;
                         }
                     }
 
-                    for(Pez pez : peces){
-                        if(pez.isVivo() && !pez.isAlimentado()){
+                    for (Pez pez : peces) {
+                        if (pez.isVivo() && !pez.isAlimentado()) {
                             pez.setAlimentado(true);
                         }
                     }
+                } else{
+                    alimentarAleatorio(cantidadDeComidaNecesariaPorPez, comidaAnimal + comidaVegetal);
                 }
             }
         }
     }
 
-    private void alimentarAleatorio(ArrayList<Integer> cantidadDeComidadNecesariaPorPez, int comidadNecesaria){
+    /**
+     * Gestiona la lógica para alimentar a los peces cuando la comida es insuficiente.
+     * @param cantidadDeComidaNecesariaPorPez Cantidad de comida que necesita cada Pez para alimentarse.
+     * @param comidaDisponible Comida de la que se dispone para alimentar a los peces.
+     */
+    private void alimentarAleatorio(ArrayList<Integer> cantidadDeComidaNecesariaPorPez, int comidaDisponible) {
         Random rt = new Random();
         ArrayList<Integer> posicionesPecesAlimentados = new ArrayList<>();
+        int posicionAleatoria = 0;
 
-        for(int i = 0; i < cantidadDeComidadNecesariaPorPez.size(); i++){
-            if(cantidadDeComidadNecesariaPorPez.get(i) == 0){
+        for (int i = 0; i < cantidadDeComidaNecesariaPorPez.size(); i++) {
+            if (cantidadDeComidaNecesariaPorPez.get(i) == 0) {
                 posicionesPecesAlimentados.add(i);
             }
         }
 
+        while (comidaDisponible > 0) {
+            posicionAleatoria = rt.nextInt(cantidadDeComidaNecesariaPorPez.size());
 
+            if (!posicionesPecesAlimentados.contains(posicionAleatoria)) {
+                comidaDisponible -= cantidadDeComidaNecesariaPorPez.get(posicionAleatoria);
+                cantidadDeComidaNecesariaPorPez.set(posicionAleatoria, 0);
+                posicionesPecesAlimentados.add(posicionAleatoria);
+            }
+            if (comidaDisponible == 1 && !cantidadDeComidaNecesariaPorPez.contains(1)) {
+                break;
+            }
+        }
+
+        if (peces.get(0) instanceof Carnivoro) {
+            comidaAnimal = comidaDisponible;
+          
+        } else {
+            if (peces.get(0) instanceof Filtrador) {
+                comidaVegetal = comidaDisponible;
+               
+            } else {
+                if (comidaAnimal > comidaVegetal) {
+                    comidaAnimal = 0;
+                    comidaVegetal = comidaDisponible;
+                } else {
+                    comidaVegetal = 0;
+                    comidaAnimal = comidaDisponible;
+                }
+                
+            }
+        }
+        for (Integer posicion : posicionesPecesAlimentados) {
+            if (peces.get(posicion).isVivo()) {
+                peces.get(posicion).setAlimentado(true);
+            }
+        }
     }
 
-    public void nextDay(){
+    /**
+     * Indica si hay un macho fértil en el tanque.
+     * @return True si hay un macho fértil en el tanque.
+     */
+    private boolean hayMachoFertil(){
+        for(Pez pez : peces){
+            if(!pez.isSexo() && pez.isFertil()){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Indica si hay una hembra fértil en el tanque.
+     * @return True si hay una hembra fértil en el tanque.
+     */
+    private boolean hayHembraFertil(){
+        for(Pez pez : peces){
+            if(pez.isSexo() && pez.isFertil()){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void reproducir(){
+        
+    }
+
+    public void nextDay() {
 
     }
 }
