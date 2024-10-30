3310e7c7d3f628fdc3acbb59c13e4798c8ee4614
Lois Domínguez Domínguez <stilton1267@gmail.com>
2024-10-30 16:49:03 +0100

    Añadido parámetro piscifactoría a selectTank() en Simulador y corrección de emptyTank() de Simulador.

--- Piscifactoría/src/simulador/Simulador.java
+++ Piscifactoría/src/simulador/Simulador.java
@@ -114,15 +114,13 @@ public class Simulador {
     }
 
     /**
-     * Muestra el menú de tanques de una piscifactoría seleccionada y permite al
+     * Muestra el menú de tanques de una piscifactoría y permite al
      * usuario seleccionar uno.
      * 
+     * @param piscifactoria Piscifactoría de la cúal se muestra el menú de tanques.
      * @return El índice del tanque seleccionado.
      */
-    private static int selectTank() {
-        int piscifactoriaSeleccionada = selectPisc();
-
-        Piscifactoria piscifactoria = piscifactorias.get(piscifactoriaSeleccionada - 1);
+    private static int selectTank(Piscifactoria piscifactoria) {
         ArrayList<Tanque> tanques = piscifactoria.getTanques();
 
         String[] cabeceraMenuPiscifactoria = { "----------- Tanques de " + piscifactoria.getNombre() + " -----------" };
@@ -136,9 +134,7 @@ public class Simulador {
                     + tanque.getCapacidadMaximaPeces() + " - Pez: " + nombrePez;
         }
 
-        GeneradorMenus.generarMenu(opcionesTanques, 0);
-
-        return SistemaEntrada.entradaOpcionNumerica(0, tanques.size());
+        return GeneradorMenus.generarMenuOperativo(cabeceraMenuPiscifactoria, opcionesTanques, 0, tanques.size());
     }
 
     /**
@@ -186,17 +182,18 @@ public class Simulador {
      * @param piscifactoria La piscifactoría que contiene los tanques disponibles.
      */
     public static void showTankStatus(Piscifactoria piscifactoria) {
-        int opcionTanque = selectTank();
+        int opcionTanque = selectTank(piscifactoria);
 
-        Tanque tanqueSeleccionado = piscifactoria.getTanques().get(opcionTanque - 1);
+        if(opcionTanque != 0){
+            Tanque tanqueSeleccionado = piscifactoria.getTanques().get(opcionTanque - 1);
 
-        if (tanqueSeleccionado.getPeces().isEmpty()) {
-            System.out.println("El tanque seleccionado no contiene peces.");
-        } else {
-            System.out.println("Estado de los peces en el tanque " + opcionTanque + ":");
-            tanqueSeleccionado.showFishStatus();
+            if (tanqueSeleccionado.getPeces().isEmpty()) {
+                System.out.println("El tanque seleccionado no contiene peces.");
+            } else {
+                System.out.println("Estado de los peces en el tanque " + opcionTanque + ":");
+                tanqueSeleccionado.showFishStatus();
+            }
         }
-
     }
 
     /**
@@ -823,18 +820,22 @@ public class Simulador {
      * Elimina todos los peces de un tanque, estén vivos o muertos.
      */
     private static void emptyTank() {
-
         int piscifactoriaSeleccionada = selectPisc();
-        Piscifactoria piscifactoria = piscifactorias.get(piscifactoriaSeleccionada - 1);
+        
+        if(piscifactoriaSeleccionada != 0){
+            Piscifactoria piscifactoria = piscifactorias.get(piscifactoriaSeleccionada - 1);
 
-        int tanqueSeleccionado = selectTank();
+            int tanqueSeleccionado = selectTank(piscifactoria);
 
-        Tanque tanque = piscifactoria.getTanques().get(tanqueSeleccionado - 1);
+            if(tanqueSeleccionado != 0){
+            Tanque tanque = piscifactoria.getTanques().get(tanqueSeleccionado - 1);
 
-        tanque.vaciarTanque();
+            tanque.vaciarTanque();
 
-        System.out.println("El tanque " + tanqueSeleccionado + " de la piscifactoría "
-                + piscifactoria.getNombre() + " ha sido vaciado.");
+            System.out.println("El tanque " + tanqueSeleccionado + " de la piscifactoría "
+                    + piscifactoria.getNombre() + " ha sido vaciado.");
+            }
+        }
     }
 
     /**
@@ -1115,7 +1116,13 @@ public class Simulador {
             switch(opcion){
                 case 1 -> {showGeneralStatus();}
                 case 2 -> {showSpecificStatus();}
-                case 3 -> {System.out.println("Operación no disponible.");}
+                case 3 -> {
+                    int piscifactoriaSeleccionada = selectPisc();
+
+                    if(piscifactoriaSeleccionada != 0){
+                        showTankStatus(piscifactorias.get(piscifactoriaSeleccionada - 1));
+                    }
+                }
                 case 4 -> {showStats();}
                 case 5 -> {showIctio();}
                 case 6 -> {nextDay();
