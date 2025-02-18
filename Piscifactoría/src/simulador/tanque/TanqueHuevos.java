3fc29c20b8b4148f7ffd968143f664410a076973
mirianbeta <miyibeta1413@gmail.com>
2025-02-18 02:18:18 +0100

    Implementación TanqueHuevos

--- Piscifactoría/src/simulador/tanque/TanqueHuevos.java
+++ Piscifactoría/src/simulador/tanque/TanqueHuevos.java
@@ -1,9 +1,63 @@
 package simulador.tanque;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import simulador.pez.Pez;
+import simulador.piscifactoria.Piscifactoria;
+
 public class TanqueHuevos extends Tanque{
-    public TanqueHuevos(int numeroTanque, int capacidadMaximaPeces){
+    private static final int CAPACIDAD_MAXIMA = 25;
+    private List<Pez> huevos; 
+    private Piscifactoria piscifactoria;
+
+    public TanqueHuevos(int numeroTanque, Piscifactoria piscifactoria) {
         super(numeroTanque, 25);
-        
+        this.piscifactoria = piscifactoria;
+        this.huevos = new LinkedList<>();
     }
 
+    public boolean tieneEspacio() {
+        return huevos.size() < CAPACIDAD_MAXIMA;
+    }
+
+    public void agregarHuevo(Pez huevo) {
+        if (tieneEspacio()) {
+            huevos.add(huevo);
+        }
+    }
+
+    public void procesarHuevos() {
+        List<Tanque> tanques = piscifactoria.getTanques();
+        List<Pez> huevosTransferidos = new LinkedList<>();
+    
+        for (Pez huevo : new ArrayList<>(huevos)) {
+            for (Tanque tanque : tanques) {
+                if (puedeRecibirPez(huevo)) {
+                    tanque.getPeces().add(huevo); 
+                    huevosTransferidos.add(huevo);
+                    System.out.println("Huevo de " + huevo.getNombre() + " transferido al tanque " + tanque.getNumeroTanque());
+                    break;
+                }
+            }
+        }
+        
+        huevos.removeAll(huevosTransferidos); 
+    }
+    
+    public boolean puedeRecibirPez(Pez pez) {
+        // Verificar espacio
+        if (getCapacidadMaximaPeces() - getPeces().size() <= 0) {
+            return false;
+        }
+    
+        // Verificar raza 
+        if (!getPeces().isEmpty() && !getPeces().get(0).getNombre().equals(pez.getNombre())) {
+            return false;
+        }
+    
+        return true;
+    }
+    
 }
